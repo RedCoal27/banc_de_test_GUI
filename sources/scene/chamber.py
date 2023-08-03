2081dfb14a67d2411b56ab238afe580577fddc24
RedCoal27 <alxandre.toupie@gmail.com>
2023-08-03 18:00:29 +0200

    test

--- sources/scene/chamber.py
+++ sources/scene/chamber.py
@@ -7,7 +7,7 @@ class Chamber(CustomWidget):
         ratio = (0.4, 0.2)
         super().__init__(parent.translator, pos, ratio, "#00B0F0", font_size=12)
         self.create_label("chamber", alignment = Qt.AlignmentFlag.AlignCenter)
-
+        
         
     def set_font_size(self,size):
         self.font_size = size + 2
