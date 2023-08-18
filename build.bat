6c9441fef55b5df574359b165c1bc6cdcb413e40
RedCoal27 <alxandre.toupie@gmail.com>
2023-08-18 09:25:16 +0200

    copie des fichier config et recettes

--- build.bat
+++ build.bat
@@ -1,1 +1,5 @@
+rmdir /s /q dist
 pyinstaller --onefile --add-data "./sources/images/*;./images" --add-data "./sources/lang/*;./lang" --icon=sources/images/xfab.jpg -F .\sources\gui.py
+
+xcopy .\recipes .\dist\recipes /E /I /Y
+copy  .\config.json .\dist\config.json
